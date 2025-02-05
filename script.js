6000b281d87417c096b1192ae08d6408c2ba4f5d
abuArsalan <arsalanshafi911@gmail.com>
2025-02-05 15:13:08 +0530

    adedd functionality

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 const textbox = document.querySelector(".txt");
 const button = document.querySelector(".btn");
 
+
+button.addEventListener("click",()=>{
+    getData(textbox.value);
+})
+
+
+
+
+
+
+async function getData(user){
+    console.log("initialized");
+    const res = await fetch(`https://api.github.com/users/${user}`);
+    const data = await res.json();
+    console.log(data.name,data.followers,data.following);
+}
