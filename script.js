1bb2e5d5015457568f528748743c445977389c10
abuArsalan <arsalanshafi911@gmail.com>
2025-02-05 15:21:14 +0530

    commit

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const textbox = document.querySelector(".txt");
 const button = document.querySelector(".btn");
+const content = document.querySelector(".content")
 
 
-button.addEventListener("click",()=>{
-    getData(textbox.value);
+button.addEventListener("click",async ()=>{
+    const data = await getData(textbox.value);
+    displayInfo(data)
 })
-
-
-
 
 
 
@@ -15,5 +14,13 @@ async function getData(user){
     console.log("initialized");
     const res = await fetch(`https://api.github.com/users/${user}`);
     const data = await res.json();
-    console.log(data.name,data.followers,data.following);
+    return data;
+}
+
+function displayInfo(data){
+    content.innerHTML=`
+        Name: ${data.name}<br>
+        followers: ${data.followers}<br>
+        following: ${data.following}<br>
+    `
 }
